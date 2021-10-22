1292414809c86b6860c3c444cc17e245476ba0c1
FrostBreker <frostbreker@gmail.com>
2021-10-22 17:55:27 +0200

    fix server.js

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const PORT = process.env.PORT;
 
 const app = express();
+
+//cors
+const corsOptions = {
+  origin: `${process.env.CLIENT_URL}`,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
+
+//parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //Optimized version
 app.use(express.static("client/build"));
@@ -10,6 +31,6 @@ app.get("/*", (_, res) => {
 });
 
 // server
-app.listen(5000, () => {
-  console.log(`Listening on port 5000`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
